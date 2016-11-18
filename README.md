8647672124ecf9154c50f879b2b8aa31eb5dc038
erdenbatuhan <erden.batuhan@gmail.com>
2016-11-19 01:53:28 +0300

    Folder Creation

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # credu.me
+http://credu.me/
---
+++ head.php
@@ -0,0 +1,17 @@
+<!-- ========== META Part ========== !-->
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible">
+<meta name="description" content="index">
+<meta name="author" content="beobe">
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<!-- ========== CSS Part ========== !-->
+<link href="./includes/Bootstrap/css/bootstrap.min.css" rel="stylesheet">
+<link href="./includes/Font-Awesome/css/font-awesome.min.css" rel="stylesheet">
+<link href="./includes/css/Chatron.css" rel="stylesheet">
+
+<!-- ========== Javascript Part ========== !-->
+<script src="./includes/Bootstrap/js/bootstrap.js"></script>
+<script src="./includes/js/jquery-3.1.1.min.js"></script>
