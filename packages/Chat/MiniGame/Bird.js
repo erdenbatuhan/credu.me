a967ec5d44951c72aee43d574bb10ab8c4000411
ekremcet <ekrem.cetinkaya@ozu.edu.tr>
2016-11-20 18:08:00 +0300

    Added Canvas for game

--- packages/Chat/MiniGame/Bird.js
+++ packages/Chat/MiniGame/Bird.js
@@ -1,3 +1,16 @@
 /**
  * Created by ekrem on 19.11.2016.
  */
+
+var birdConstructor = function (xPos,yPos,size,acceleration,yVelocity) {
+    this.xPos = xPos;
+    this.yPos = yPos;
+    this.frame = 0;
+
+
+
+};
+
+function jump() {
+
+}
