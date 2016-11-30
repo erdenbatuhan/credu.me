1cc038124882c2f7f776d80c1f469d24b1eeb232
ekremcet <ekrem.cetinkaya@ozu.edu.tr>
2016-11-30 21:02:24 +0300

    Getting User- Friendlist -Courses Info From DB in Profile Page

--- packages/Profile/index.php
+++ packages/Profile/index.php
@@ -7,21 +7,51 @@
 
     $connection = new mysqli($hostName, $hostUser, $hostPw, $dbName);
 
+    if (!$connection)
+        die("Connection Failed!");
     /**/
     $profileId = "S001146"; /* $_GET['id'] */
     $friendList = array();
+    $courseList = array();
+    $userInfo = array();
 
-    $friendQuery = "SELECT second_user_id FROM FRIENDSHIP";
+    /* Get the query results and store them in arrays - FriendList and CoursesTaken */
+    $userInfoQuery = "SELECT * FROM USERS WHERE ID ='".$profileId."'";
+    $userInfoQueryResult = mysqli_query($connection,$userInfoQuery);
+    $friendQuery = "SELECT SECOND_USER_ID FROM FRIENDSHIP WHERE FIRST_USER_ID ='".$profileId."'";;
     $friendQueryResult = mysqli_query($connection, $friendQuery);
+    $courseQuery = "SELECT COURSE_ID FROM COURSESTAKEN WHERE USER_ID ='".$profileId."'";;
+    $courseQueryResult = mysqli_query($connection, $courseQuery);
+
+    if(!$userInfoQueryResult){
+        throw new Exception("User Query error");
+    }
 
     if(!$friendQueryResult){
-        throw new Exception("Database error ");
+        throw new Exception("Friend List Query error ");
     }
+
+    if(!$courseQueryResult){
+        throw new Exception("Course query error");
+    }
+
 
     while ($row = mysqli_fetch_assoc($friendQueryResult)){
-        echo($row["second_user_id"]);
-        echo("bn");
+        array_push($friendList,$row["SECOND_USER_ID"]);
     }
-    mysqli_close($connection);
 
+    while ($row = mysqli_fetch_assoc($courseQueryResult)){
+        array_push($courseList,$row["COURSE_ID"]);
+    }
+
+    echo("Friends: "."<br/>");
+    foreach($friendList as $key => $value){
+        echo $key.":".$value."<br/>";
+    }
+    echo("Courses: "."<br/>");
+    foreach($courseList as $key => $value){
+    echo $key.":".$value."<br/>";
+}
+    mysqli_close($connection);
+    /**/
 ?>
