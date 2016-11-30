0a75b77f244ca934a60782008244fda326c421c0
erdenbatuhan <erden.batuhan@gmail.com>
2016-11-30 21:28:42 +0300

    deleted un-necessary constants..

--- packages/Chat/DatabaseConnection.php
+++ packages/Chat/DatabaseConnection.php
@@ -1,10 +1,5 @@
 <?php
 
-const HOST_NAME = "localhost";
-const HOST_USER = "root";
-const HOST_PASSWORD = "3448";
-const DATABASE_NAME = "credu.me";
-
 class DatabaseConnection {
 
     private $connection = null;
@@ -19,7 +14,7 @@ class DatabaseConnection {
 
     public function initiateConnection() {
         $this->connection = new mysqli($this->hostName, $this->hostUser, $this->hostPassword, $this->databaseName);
-        
+
         if (!$this->connection)
             die("Connection Failed!");
     }
