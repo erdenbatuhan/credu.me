cc5eca4d188a88c835f8360b3d924e0ce768b9eb
erdenbatuhan <erden.batuhan@gmail.com>
2016-12-01 21:37:26 +0200

    Database is updated..

--- database/Database.sql
+++ database/Database.sql
@@ -1,12 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 4.4.10
--- http://www.phpmyadmin.net
---
--- Host: localhost:8889
--- Generation Time: Dec 01, 2016 at 09:34 PM
--- Server version: 5.5.42
--- PHP Version: 7.0.0
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
