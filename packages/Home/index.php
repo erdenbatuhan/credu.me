ebda747fcb8e9c9c6c4d8db6c768ec15acc4c309
barisceylan <baris.ceylan@ozu.edu.tr>
2016-12-02 19:49:43 +0300

    Navbar design.

--- packages/Home/index.php
+++ packages/Home/index.php
@@ -11,28 +11,51 @@
 
 
 </head>
-<body style="background-color: #ff0000;">
+<body style="background-color: #FAAC58;">
 
-<nav class="navbar navbar-inverse" style="color: #fff;border-radius:0px;
-    background-color: #669999;text-align: left; font-weight: bold; font-size: 25px; padding-top: 20px;">
-    <a href="./"> CREDU.ME </a>
-    <form class="navbar-form navbar-right">
-        <div class="form-group">
-            <input type="text" class="form-control" placeholder="Username">
-            <input type="text" class="form-control" placeholder="Password">
+<nav class="navbar navbar-default">
+    <div class="container-fluid">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="./">Credu.me</a>
         </div>
-        <button type="submit" class="btn btn-default">Sign In</button>
-    </form>
 
-
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav">
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Creators <span class="caret"></span></a>
+                    <ul class="dropdown-menu">
+                        <li><a href="https://github.com/barisceylan">Barış Ceylan</a></li>
+                        <li><a href="https://github.com/erdenbatuhan">Batuhan Erden</a></li>
+                        <li><a href="https://github.com/ekremcet">Ekrem Çetinkaya</a></li>
+                        <li><a href="https://github.com/ErkAydogan">Erk Aydoğan</a></li>
+                        <li><a href="https://github.com/obukte">Ömer Said Bükte</a></li>
+                    </ul>
+                </li>
+            </ul>
+            <form class="navbar-form navbar-right">
+                <div class="form-group">
+                    <input type="text" class="form-control" placeholder="Username">
+                    <input type="text" class="form-control" placeholder="Password">
+                </div>
+                <button type="submit" class="btn btn-default">Sign In</button>
+            </form>
+        </div><!-- /.navbar-collapse -->
+    </div><!-- /.container-fluid -->
 </nav>
-
-<div class="container">
+<div class="container " >
 
 
     <tr>
         <td width="40%">
-            <form action="#" method="POST">
+            <form action="#" method="POST" >
                 <h1>JOIN US !</h1>
                 <input type="text" name="username" size="25" placeholder="Username" /><br /> <br />
                 <input type="text" name="password" size="25" placeholder="Password" /><br /> <br />
