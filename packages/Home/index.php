090da32ebd0b7c599b8da0dab87d0392f7d58953
barisceylan <baris.ceylan@ozu.edu.tr>
2016-12-02 20:46:02 +0300

    Adding css link.

--- packages/Home/index.php
+++ packages/Home/index.php
@@ -5,24 +5,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+    <link rel="stylesheet"  href="homepage.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
 
 
 </head>
-<body style="background-color: #FAAC58;">
-
-<nav class="navbar navbar-default">
+<body>
+<nav class="navbar navbar-default navbar-inverse">
     <div class="container-fluid">
         <!-- Brand and toggle get grouped for better mobile display -->
         <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
             <a class="navbar-brand" href="./">Credu.me</a>
         </div>
 
@@ -45,11 +39,14 @@
                     <input type="text" class="form-control" placeholder="Username">
                     <input type="text" class="form-control" placeholder="Password">
                 </div>
-                <button type="submit" class="btn btn-default">Sign In</button>
+                <button type="submit" class="btn btn-primary">Sign In</button>
             </form>
         </div><!-- /.navbar-collapse -->
     </div><!-- /.container-fluid -->
 </nav>
+
+
+
 <div class="container " >
 
 
@@ -67,5 +64,6 @@
     </tr>
 
 </div>
+
 
 </body>
