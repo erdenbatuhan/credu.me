a0abdcd719a20640cad8cfefe5f4b43f07218b3f
erdenbatuhan <erden.batuhan@gmail.com>
2016-12-02 23:02:48 +0300

    User class is filled with functions that are ready to be implemented

--- packages/Chat/User.php
+++ packages/Chat/User.php
@@ -2,7 +2,36 @@
 
 class User {
 
-    public function __construct() {
+    /** @var DatabaseConnection */
+    private $databaseConnection = null;
+
+    private $userId = null;
+    private $firstName = null;
+    private $lastName = null;
+    private $username = null;
+    private $email = null;
+    private $phoneNumber = null;
+    private $universityName = null;
+    private $friends = null;
+    private $coursesTaken = null;
+
+    public function __construct($databaseConnection, $userId) {
+        $this->databaseConnection = $databaseConnection;
+        $this->userId = $userId;
+
+        $this->friends = array();
+        $this->coursesTaken = array();
+    }
+
+    public function addFriend($username) {
+
+    }
+
+    public function removeFriend($username) {
+
+    }
+
+    public function joinChatRoom($chatRoomId) {
 
     }
 }
