5bea18b866dd6f9b8da1d2be7f1784c23cf6fced
barisceylan <baris.ceylan@ozu.edu.tr>
2016-12-11 21:45:46 +0300

    Edit profile page design.

---
+++ packages/Profile/editprofile.php
@@ -0,0 +1,67 @@
+<html>
+<head>
+    <title> Edit Profile | credu.me </title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+    <link rel="stylesheet"  href="../../includes/css/Editprofile.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+</head>
+<body>
+<nav class="navbar navbar-default navbar-inverse">
+    <div class="container-fluid">
+
+        <div class="navbar-header">
+            <a class="navbar-brand" href="/credu.me/packages/Home/index.php">Credu.me</a>
+        </div>
+
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav">
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Creators <span class="caret"></span></a>
+                    <ul class="dropdown-menu">
+                        <li><a href="https://github.com/barisceylan">Barış Ceylan</a></li>
+                        <li><a href="https://github.com/erdenbatuhan">Batuhan Erden</a></li>
+                        <li><a href="https://github.com/ekremcet">Ekrem Çetinkaya</a></li>
+                        <li><a href="https://github.com/ErkAydogan">Sayım Erk Aydoğan</a></li>
+                        <li><a href="https://github.com/obukte">Ömer Said Bükte</a></li>
+                    </ul>
+                </li>
+            </ul>
+            <ul class="navbar-form navbar-right">
+
+                <button type="button" onclick= "/credu.me/packages/Home/index.php"class="btn btn-default btn-sm">
+                    <span class="glyphicon glyphicon-log-out"></span> Log Out
+                </button>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+<div class="editbox">
+    <form>
+        <div class="form-input">
+            <h3>EDIT PROFILE</h3>
+            <br>
+            <input type="text" class="form-control" placeholder="Name" />
+            <br>
+            <input type="text" class="form-control" placeholder="Email" />
+            <br>
+            <input type="text" class="form-control" placeholder="Phone Number" />
+            <br>
+            <button formaction="/credu.me/packages/Profile/index3.php" type="submit" class="btn btn-default">Edit</button>
+    </form>
+
+</div>
+
+
+<div class="footer">Credu.me &copy; 2016</strong></div>
+</body>
+
+</html>
