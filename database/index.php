cc40e35d99fbc1281e055aad5daa773c96d60580
erdenbatuhan <erden.batuhan@gmail.com>
2016-12-13 21:45:15 +0300

    Every directory has its own index.php

---
+++ database/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title> credu.me </title>
+</head>
+<body onload="redirect();">
+</body>
+<script>
+    function redirect() {
+        window.location.href = "../packages/Home/";
+    }
+</script>
+</html>
