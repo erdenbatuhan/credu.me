323d65802adebf412aa5f9f3702b940f8e4ec799
erdenbatuhan <erden.batuhan@gmail.com>
2016-12-13 19:18:13 +0300

    testing server

--- index.php
+++ index.php
@@ -1,29 +1,25 @@
 <!DOCTYPE html>
-<html lang="tr">
+<html lang="en">
 <head>
     <title> credu.me </title>
-    <?php "head.php"; ?>
 </head>
-<body id="chat-index">
-<br><br><br>
-<div class="jumbotron container">
-    <?php
+<body>
+<?php
 
-    include "packages/DatabaseConnection.php";
+include "packages/DatabaseConnection.php";
 
-    $db = new DatabaseConnection();
-    $db->initiateConnection();
-    $connection = $db->getConnection();
+$db = new DatabaseConnection();
+$db->initiateConnection();
+$connection = $db->getConnection();
 
-    $sql_query = "SELECT * FROM USERS";
-    $sql_result = mysqli_query($connection, $sql_query);
+$sql_query = "SELECT * FROM USERS";
+$sql_result = mysqli_query($connection, $sql_query);
 
-    while ($row = mysqli_fetch_assoc($sql_result)) {
-        echo $row["FIRST_NAME"] . " " . $row["LAST_NAME"] . "<br>";
-    }
+while ($row = mysqli_fetch_assoc($sql_result)) {
+    echo $row["FIRST_NAME"] . " " . $row["LAST_NAME"] . "<br>";
+}
 
-    $db->killConnection();
-    ?>
-</div>
+$db->killConnection();
+?>
 </body>
 </html>
