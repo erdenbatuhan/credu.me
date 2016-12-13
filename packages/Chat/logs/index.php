6c3a0e04c2943229b0626a5e4fb3c01498ff8dc0
erdenbatuhan <erden.batuhan@gmail.com>
2016-12-13 21:34:13 +0300

    Directories that should be hidden are now hidden

---
+++ packages/Chat/logs/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title> credu.me </title>
+</head>
+<body onload="redirect();">
+</body>
+<script>
+    function redirect() {
+        window.location.href = "../../Home";
+    }
+</script>
+</html>
