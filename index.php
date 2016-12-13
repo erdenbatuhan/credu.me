d1285f1b76e8c5345cb4e28d7569f3b9ee444f25
erdenbatuhan <erden.batuhan@gmail.com>
2016-12-13 21:20:30 +0300

    Tests are complete!

--- index.php
+++ index.php
@@ -3,23 +3,11 @@
 <head>
     <title> credu.me </title>
 </head>
-<body>
-<?php
-
-include "packages/DatabaseConnection.php";
-
-$db = new DatabaseConnection();
-$db->initiateConnection();
-$connection = $db->getConnection();
-
-$sql_query = "SELECT * FROM USERS";
-$sql_result = mysqli_query($connection, $sql_query);
-
-while ($row = mysqli_fetch_assoc($sql_result)) {
-    echo $row["FIRST_NAME"] . " " . $row["LAST_NAME"] . "<br>";
-}
-
-$db->killConnection();
-?>
+<body onload="redirect();">
 </body>
+<script>
+    function redirect() {
+        window.location.href = "./packages/Home/";
+    }
+</script>
 </html>
