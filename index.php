61ff881338d219d01f01852c4e56a3063f937415
erdenbatuhan <erden.batuhan@gmail.com>
2016-12-13 19:08:50 +0300

    testing server

--- index.php
+++ index.php
@@ -7,7 +7,25 @@
 <body id="chat-index">
 <br><br><br>
 <div class="jumbotron container">
-    <h5> Hello World! </h5>
+    <?php
+
+    include "packages/DatabaseConnection.php";
+
+    $db = new DatabaseConnection();
+    $db->initiateConnection();
+    $connection = $db->getConnection();
+
+    $sql_query = "SELECT * FROM USERS";
+    $sql_result = mysqli_query($connection, $sql_query);
+    $c = mysqli_num_rows($sql_result);
+    echo c;
+
+    while ($row = mysqli_fetch_assoc($sql_result)) {
+        echo $row["FIRST_NAME"] . " " . $row["LAST_NAME"] . "<br>";
+    }
+
+    $db->killConnection();
+    ?>
 </div>
 </body>
 </html>
